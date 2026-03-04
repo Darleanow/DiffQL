582b4a1737125fdc385b84ca076c58077a91494a
JørR <43743804+Ezyrone@users.noreply.github.com>
2026-03-04 12:36:13 +0100

    feat(PostGreSQL): Add connector postgresql file

---
+++ src/Connectors/PostGreSQL/Connector.cpp
@@ -0,0 +1,9 @@
+
+#include "SQLParser.h"
+#include "SQLParserResult.h"
+#include "sql/SQLStatement.h"
+#include "sql/SelectStatement.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
