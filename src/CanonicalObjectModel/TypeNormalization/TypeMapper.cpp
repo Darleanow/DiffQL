a3eb34e45196e867b53f6452f7fa72c8d6c4d0fc
Darleanow <enzohugonn@gmail.com>
2026-03-05 13:37:03 +0100

    wip(Project): Commit WIP

--- src/CanonicalObjectModel/TypeNormalization/TypeMapper.cpp
+++ src/CanonicalObjectModel/TypeNormalization/TypeMapper.cpp
@@ -1,11 +1,11 @@
-#include "DiffQL/CanonicalObjectModel/Hierarchy.hpp"
 #include "DiffQL/CanonicalObjectModel/TypeNormalization/TypeMapper.hpp"
+#include "DiffQL/CanonicalObjectModel/Hierarchy.hpp"
 
 CanonicalType MariaDBTypeMapper::map(const std::string &raw)
 {
   if(raw.starts_with("int")) {
-    return {CanonicalType::INTEGER, {}, {}, {}, raw};
+    // return {CanonicalType::INTEGER, {}, {}, {}, raw};
   }
 
-  return {CanonicalType::INTEGER, {}, {}, {}, raw};
+  return {CanonicalType::INTEGER, {}, {}, {}};
 }
