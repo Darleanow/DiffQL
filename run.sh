3da81260a2dba01a7e961031264f4717e17b535d
Darleanow <enzohugonn@gmail.com>
2026-03-06 15:28:31 +0100

    fix(run.sh): Remove incorrect port specification from DiffQL command

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd build
-./DiffQL --source -u root -p toor -d nation --target -u root -p toor -d nation2 -P 3307
+./DiffQL --source -u root -p toor -d nation --target -u root -p toor -d nation2
